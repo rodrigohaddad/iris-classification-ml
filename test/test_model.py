4119a064f493ab153d83987aafc295ca46805cea
rodrigohaddad <rgh@poli.ufrj.br>
2023-11-08 21:57:10 -0300

    feature: automatic deploy

--- test/test_model.py
+++ test/test_model.py
@@ -11,6 +11,7 @@ from trainer.model import Model
 class TestModelCase(unittest.TestCase):
     def setUp(self):
         self.current_directory = os.getcwd()
+        print(self.current_directory)
         self.output = os.path.join(self.current_directory, 'output')
         self.eval = os.path.join(self.current_directory, 'input', 'eval.csv')
         self.train = os.path.join(self.current_directory, 'input', 'train.csv')
